30d51010abb9eaaea7c1ee11be5204794e53c1ad
RomanAfanasev <45339525+RomanAfanasev@users.noreply.github.com>
2021-08-26 17:43:24 +0300

    Updates

---
+++ precache-manifest.9a0da80e09921686cfd4cc1d946e9465.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587cac4cfd94ed1ed18f2b521e049112",
+    "url": "/DeployTest/index.html"
+  },
+  {
+    "revision": "c6338c5b71878c88698c",
+    "url": "/DeployTest/static/css/main.6f42b3fa.chunk.css"
+  },
+  {
+    "revision": "4cc1e8b3f4d3b9b671df",
+    "url": "/DeployTest/static/js/2.d2f28b14.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/DeployTest/static/js/2.d2f28b14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6338c5b71878c88698c",
+    "url": "/DeployTest/static/js/main.494da3da.chunk.js"
+  },
+  {
+    "revision": "a403aeb64d8f5d2d260c",
+    "url": "/DeployTest/static/js/runtime-main.ccccefb4.js"
+  },
+  {
+    "revision": "f646ab658cea3efc6b9cadb38686d6b4",
+    "url": "/DeployTest/static/media/flytech6.f646ab65.svg"
+  }
+]);
