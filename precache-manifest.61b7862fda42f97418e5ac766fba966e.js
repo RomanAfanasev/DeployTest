899b7a8fc4ebcfcf38470e2e21723db783b9f98f
RomanAfanasev <45339525+RomanAfanasev@users.noreply.github.com>
2021-08-31 14:11:42 +0300

    Updates

--- precache-manifest.61b7862fda42f97418e5ac766fba966e.js
+++ precache-manifest.61b7862fda42f97418e5ac766fba966e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "875b1e75f899a9d940bdbb08a5236b1c",
+    "revision": "e308827eb6d5f135d5ab95899565601a",
     "url": "/DeployTest/index.html"
   },
   {
-    "revision": "4da7c3921b038c15808c",
-    "url": "/DeployTest/static/css/main.6f42b3fa.chunk.css"
+    "revision": "6248b17f4230f17ec91f",
+    "url": "/DeployTest/static/css/main.f25baa3e.chunk.css"
   },
   {
     "revision": "be0939c9d30723dcf1ae",
@@ -16,12 +16,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/DeployTest/static/js/2.cd82ec96.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4da7c3921b038c15808c",
-    "url": "/DeployTest/static/js/main.7a597ff7.chunk.js"
+    "revision": "6248b17f4230f17ec91f",
+    "url": "/DeployTest/static/js/main.49d45cd3.chunk.js"
   },
   {
     "revision": "a403aeb64d8f5d2d260c",
     "url": "/DeployTest/static/js/runtime-main.ccccefb4.js"
+  },
+  {
+    "revision": "5b52596b9f3509202e32c59213345946",
+    "url": "/DeployTest/static/media/bg_9.5b52596b.jpg"
   },
   {
     "revision": "f646ab658cea3efc6b9cadb38686d6b4",
