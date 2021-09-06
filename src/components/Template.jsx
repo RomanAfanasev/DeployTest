f1ecc5052a5286c5d34456f365ae27b24c594d8b
RomanAfanasev <45339525+RomanAfanasev@users.noreply.github.com>
2021-09-06 12:33:40 +0300

    add contact component

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 //import logo from '../images/flytech6.svg';
 import WorkModal from './for-new-template/WorkModal';
+import ContactModal from './for-new-template/ContactModal';
 
 export default function NewTemplate() {
 
     //For hidden items
-    const [showResults, setShowResults] = React.useState(false)
-    const onClick = () => setShowResults(!showResults)
+    const [showResultsWork, setShowResultsWork] = React.useState(false)
+    const onClickWork = () => setShowResultsWork(!showResultsWork)
+
+    const [showResultsContact, setShowResultsContact] = React.useState(false)
+    const onClickContact = () => setShowResultsContact(!showResultsContact)
 
 
     return (
@@ -27,14 +31,15 @@ export default function NewTemplate() {
                 <nav>
                     <ul>
                         <li>
-                            <a href="#intro">
-                                Intro
+                            <a 
+                                onClick={onClickContact} href="#contact">
+                                Contact
                             </a>
                         </li>
-                        <li><a aria-controls="video-modal-intro"
-                            onClick={onClick} href="#work">
-                                Work
-                            </a>
+                        <li><a 
+                            onClick={onClickWork} href="#work">
+                            Work
+                        </a>
                         </li>
                         <li><a href="#about">About</a></li>
                     </ul>
@@ -44,22 +49,11 @@ export default function NewTemplate() {
             {/* Main */}
             <div id="main">
 
-                {/* Intro  */}
-                {/* <article id="intro">
-                    <h2 className="major">Intro</h2>
-                    <span className="image main"><img src="images/pic01.jpg" alt="" /></span>
-                    <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
-                </article> */}
+                {/* Contact  */}                
+                {showResultsContact ? <ContactModal /> : null}
 
                 {/* Work  */}
-                {/* <article id="work">
-                    <h2 className="major">Work</h2>
-                    <span className="image main"><img src="images/pic02.jpg" alt="" /></span>
-                    <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-                    <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
-                </article> */}
-                { showResults ? <WorkModal /> : null }
+                {showResultsWork ? <WorkModal /> : null}
 
                 {/* About */}
                 {/* <article id="about">
---
+++ src/components/for-new-template/ContactModal.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function ContactModal() {
+
+    return (
+        // Modal
+        <article id="contact">
+            <h2 className="major">Contact</h2>            
+            <p>oursmail@gmail.com</p>
+            <p>88005553535</p>
+        </article>
+
+    );
+}
