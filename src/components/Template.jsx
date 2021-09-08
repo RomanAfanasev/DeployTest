16c92a3af1f06fb6ad40b220403af4a872dbe0bb
RomanAfanasev <45339525+RomanAfanasev@users.noreply.github.com>
2021-09-08 13:54:23 +0300

    Some design changes

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -2,26 +2,42 @@ import React, { useState } from 'react';
 //import logo from '../images/flytech6.svg';
 import WorkModal from './for-new-template/WorkModal';
 import ContactModal from './for-new-template/ContactModal';
-import AboutModal from './AboutModal';
+import AboutModal from './for-new-template/AboutModal';
 
 export default function NewTemplate() {
 
     //For hidden items
     const [showResultsContact, setShowResultsContact] = React.useState(false)
-    const onClickContact = () => setShowResultsContact(!showResultsContact)
+    //const onClickContact = () => setShowResultsContact(!showResultsContact)
+    const onClickContact = function () {
+        setShowResultsContact(!showResultsContact);
+        setShowResultsWork(false);
+        setShowResultsAbout(false);
+
+    }
 
     const [showResultsWork, setShowResultsWork] = React.useState(false)
-    const onClickWork = () => setShowResultsWork(!showResultsWork)
+    //const onClickWork = () => setShowResultsWork(!showResultsWork)
+    const onClickWork = function(){
+        setShowResultsContact(false);
+        setShowResultsWork(!showResultsWork);
+        setShowResultsAbout(false);
+    }
 
     const [showResultsAbout, setShowResultsAbout] = React.useState(false)
-    const onClickAbout = () => setShowResultsAbout(!showResultsAbout)
+    //const onClickAbout = () => setShowResultsAbout(!showResultsAbout)
+    const onClickAbout = function(){
+        setShowResultsContact(false);
+        setShowResultsWork(false);
+        setShowResultsAbout(!showResultsAbout);
+    }
 
-    const onClickMain = function(){
+    const onClickMain = function () {
         setShowResultsContact(false);
         setShowResultsWork(false);
         setShowResultsAbout(false);
     }
-    
+
 
     return (
         // Wrapper
@@ -41,12 +57,12 @@ export default function NewTemplate() {
                 <nav>
                     <ul>
                         <li>
-                            <a 
+                            <a
                                 onClick={onClickContact} >
                                 Contact
                             </a>
                         </li>
-                        <li><a 
+                        <li><a
                             onClick={onClickWork} >
                             Work
                         </a>
@@ -59,13 +75,13 @@ export default function NewTemplate() {
             {/* Main */}
             <div id="main" onClick={onClickMain}>
 
-                {/* Contact  */}                
+                {/* Contact  */}
                 {showResultsContact ? <ContactModal /> : null}
 
                 {/* Work  */}
                 {showResultsWork ? <WorkModal /> : null}
 
-                {/* About */}                
+                {/* About */}
                 {showResultsAbout ? <AboutModal /> : null}
 
             </div>
--- src/components/for-new-template/WorkModal.jsx
+++ src/components/for-new-template/WorkModal.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import workImg from '../../images/ssp063.jpg'
+
 export default function WorkModal() {
 
     return (
@@ -7,12 +9,7 @@ export default function WorkModal() {
         <article id="work">
             <h2 className="major">Work</h2>
             <span className="image main">
-                <iframe src='https://www.youtube.com/embed/BoiNyuH1-8o'
-                    frameborder='0'
-                    allow='autoplay; encrypted-media'
-                    allowfullscreen
-                    title='video'
-                />
+                <img src= {workImg} ></img>
             </span>
             <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
             <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
